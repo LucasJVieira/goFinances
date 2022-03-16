5f4de6c3e9934ccbbd00677a6151daf24aa6d20f
LucasVieira <lucasjfvieira@gmail.com>
2022-03-15 22:17:49 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return <Dashboard />
+}
+
