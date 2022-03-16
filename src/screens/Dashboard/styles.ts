9c4b9e7cc825ff474357eef7df89f98f4de8377a
LucasVieira <lucasjfvieira@gmail.com>
2022-03-15 23:17:09 -0300

    Criação dos componentes Title e Container

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View `
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background: #FFFF;
+    ;
+`;
+
+export const Title = styled.Text `
+    font-size: 24px;
+    font-weight: bold;
+    color: purple;
+`;
